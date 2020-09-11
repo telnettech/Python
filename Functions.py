d641db2c6fc20ac8c7e404a3eef68b87538a5d15
Brian Simpson <telnettech@gmail.com>
2020-09-10 20:13:11 -0400

    Completed Function Section of Treehouse Track

--- Functions.py
+++ Functions.py
@@ -11,5 +11,64 @@ def set_num():
     print(num)
 
 
+# Getting value out of a function
+def two_plus_two():
+    val = 2 + 2
+    return val
+
+
+# Passing Argument to Function
+def add_two(num):
+    val = num + 2
+    return val
+
+
+def multi_param(num1, num2):
+    val = num1 + num2
+    return val
+
+
+# Packing a Function
+def packer(*args):
+    print(args)
+
+
+# Packing a function with a for loop
+def packer_loop(*args):
+    for val in args:
+        print(val)
+
+
+# def calculate_total(*args):
+#    total = sum(args)
+#    print(total)
+
+# Using function to unpack variables
+def unpacker():
+    return (1, 2, 3)
+
+
+def unpacker2():
+    return 'hey'
+
+
 print_fav_movie()
 set_num()
+print(two_plus_two())
+sum = two_plus_two()
+print(sum)
+print(two_plus_two() * 2)
+add_two(5)
+print(add_two(5))  # Parameter 'num' from function needed within print statement
+print(multi_param(5, 10))  # Both parameters needed for function to work.
+packer('hi', 'I', 'love', 'python')
+packer_loop('hi', 'I', 'love', 'python')  # Prints on individual lines
+# calculate_total(25, 25, 20, 30)
+var1, var2, var3 = unpacker() # Takes function and puts values to the variables
+print(var1)
+print(var2)
+print(var3)
+var1, var2, var3 = unpacker2()
+print(var1)
+print(var2)
+print(var3)
---
+++ test.py
@@ -0,0 +1,8 @@
+def unpacker():
+    return (1, 2, 3)
+
+
+var1, var2, var3 = unpacker()
+print(var1)
+print(var2)
+print(var3)
